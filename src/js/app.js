cc623a89cd5728b0927995f8955b3c4ba1863f4d
Andrew <alevy@trevetts.com>
2018-05-11 16:10:56 -0400

    delete some stuff

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,31 @@
 (function() {
-	'use strict';
+    'use strict';
 
-	//Create a app level module which has dependencies on controllers and components
-	var ngPrototype =
-		angular.module('ngSeedApp', [
-			'ui.router',
-			'ngSeedApp.controllers'
-		]);
+    //Create a app level module which has dependencies on controllers and components
+    var ngPrototype =
+        angular.module('ngSeedApp', [
+            'ui.router',
+            'ngSeedApp.controllers'
+        ]);
 
-	ngPrototype.run(['$rootScope', '$state', '$stateParams',
-			function($rootScope, $state, $stateParams) {
+    ngPrototype.run(['$rootScope', '$state', '$stateParams',
+            function($rootScope, $state, $stateParams) {
 
-				//Add references to $state and $stateParams to the $rootScope
-				$rootScope.$state = $state;
-				$rootScope.$stateParams = $stateParams;
-			}
-		])
-		.config(['$stateProvider', '$urlRouterProvider',
-			function($stateProvider, $urlRouterProvider) {
+                //Add references to $state and $stateParams to the $rootScope
+                $rootScope.$state = $state;
+                $rootScope.$stateParams = $stateParams;
+            }
+        ])
+        .config(['$stateProvider', '$urlRouterProvider',
+            function($stateProvider, $urlRouterProvider) {
 
-				$urlRouterProvider.otherwise('/');
+                $urlRouterProvider.otherwise('/');
 
-
-				$stateProvider.state("home", {
-					url: "/",
-					controller: 'homeController',
-					templateUrl: 'src/js/subapps/home/template.html'
-				}).state("about", {
-					url: "/about",
-					controller: 'aboutController',
-					templateUrl: 'src/js/subapps/about/template.html'
-				});
-			}
-		]);
+                $stateProvider.state('home', {
+                    url: '/',
+                    controller: 'homeController',
+                    templateUrl: 'src/js/subapps/home/template.html'
+                });
+            }
+        ]);
 }());
