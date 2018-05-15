3a656ed4389ab391ee2b19c1cc489463b11a68f4
Andrew <alevy@trevetts.com>
2018-05-15 17:22:17 -0400

    start of setup

--- src/js/subapps/home/controller.js
+++ src/js/subapps/home/controller.js
@@ -1,10 +1,12 @@
 (function() {
-	'use strict';
-	angular.module('ngSeedApp.controllers')
-		.controller('homeController', [
-			'$scope',
-			function($scope) {
-				$scope.title = "Welcome";
-			}
-		]);
+    'use strict';
+    angular.module('ngSeedApp.controllers', ['ui.sortable'])
+        .controller('homeController', ['$scope',
+            function($scope) {
+                $scope.setup = true;
+                $scope.roundEnd = function() {
+                    console.log('round overrr');
+                };
+            }
+        ]);
 }());
