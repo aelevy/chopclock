e344a08a664df5c72231b32a75e716b3bc14da16
Andrew <alevy@trevetts.com>
2018-05-31 12:47:58 -0400

    level work

--- js/subapps/home/controller.js
+++ js/subapps/home/controller.js
@@ -7,6 +7,30 @@
                 tournament.setup = true;
                 tournament.prizePayouts = Array(5);
                 tournament.settings = [];
+                tournament.levels = [
+                  {},{},{},{},{},{},{},{},{},{},
+                  {},{},{},{},{},{},{},{},{},{},
+                  {},{},{},{},{},{},{},{},{},{}
+                ];
+
+                $scope.list = tournament.levels;
+                // $scope.sortingLog = [];
+                //
+                // $scope.sortableOptions = {
+                //   update: function(e, ui) {
+                //     var logEntry = tournament.levels.map(function(i){
+                //       return i.value;
+                //     }).join(', ');
+                //     $scope.sortingLog.push('Update: ' + logEntry);
+                //   },
+                //   stop: function(e, ui) {
+                //     // this callback has the changed model
+                //     var logEntry = tournament.levels.map(function(i){
+                //       return i.value;
+                //     }).join(', ');
+                //     $scope.sortingLog.push('Stop: ' + logEntry);
+                //   }
+                // };
                 tournament.roundEnd = function() {
                     console.log('round overrr');
                 };
